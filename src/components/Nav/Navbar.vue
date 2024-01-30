683712c91b44542ce79ac9ad8b9f20cca7ca5fe6
Chen Prom <cprom@Chens-MBP.lan>
2024-01-29 21:51:12 -0600

    update to routes in navbar

--- src/components/Nav/Navbar.vue
+++ src/components/Nav/Navbar.vue
@@ -4,12 +4,12 @@ import './Navbar.css'
 
 <template>
    <nav class="nav-container">
-        <router-link class="home-icon" to="/"><font-awesome-icon class="lightbulb-icon" icon="fa-solid fa-lightbulb" size="2xl" style="color: #63E6BE;" /><font-awesome-icon class="brain-icon" icon="fa-solid fa-brain" size="2xl" style="color: #FFD43B ;" /></router-link>
+        <router-link class="home-icon" to="/learn-to-read/home"><font-awesome-icon class="lightbulb-icon" icon="fa-solid fa-lightbulb" size="2xl" style="color: #63E6BE;" /><font-awesome-icon class="brain-icon" icon="fa-solid fa-brain" size="2xl" style="color: #FFD43B ;" /></router-link>
          <div>
-            <button class="reading button"><router-link to="/read"><font-awesome-icon icon="fa-solid fa-book" size="xl" style="color: #63E6BE;" /> Reading</router-link></button>
-            <button class="writing button"><router-link to="/write"><font-awesome-icon icon="fa-solid fa-pen" size="xl" style="color: #B197FC;" /> Writing</router-link></button>
-            <button class="math button"><router-link to="/math"><font-awesome-icon icon="fa-solid fa-square-root-variable" size="xl" style="color: #ff8ae9;" /> Math</router-link></button>
+            <button class="reading button"><router-link to="/learn-to-read/read"><font-awesome-icon icon="fa-solid fa-book" size="xl" style="color: #63E6BE;" /> Reading</router-link></button>
+            <button class="writing button"><router-link to="/learn-to-read/write"><font-awesome-icon icon="fa-solid fa-pen" size="xl" style="color: #B197FC;" /> Writing</router-link></button>
+            <button class="math button"><router-link to="/learn-to-read/math"><font-awesome-icon icon="fa-solid fa-square-root-variable" size="xl" style="color: #ff8ae9;" /> Math</router-link></button>
          </div>
-        <router-link to="/settings"><font-awesome-icon class="setting-icon" icon="fa-solid fa-gear" size="2xl" style="color: #B197FC;" /></router-link>
+        <router-link to="/learn-to-read/settings"><font-awesome-icon class="setting-icon" icon="fa-solid fa-gear" size="2xl" style="color: #B197FC;" /></router-link>
    </nav>
 </template>
