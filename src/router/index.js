539c7b14edb753489f22daf5ff6ff51ee6454519
Chen Prom <cprom@Chens-MBP.lan>
2024-02-02 09:45:17 -0600

    router bug

--- src/router/index.js
+++ src/router/index.js
@@ -11,37 +11,33 @@ const routes = [
         path: `/learn-to-read/home`,
         name: home,
         component: home,
-        mode: 'hash'
     },
     
     {
         path: '/learn-to-read/read',
         name: read,
         component: read,
-        mode: 'hash'
     },
     {
         path: '/learn-to-read/write',
         name: write,
         component: write,
-        mode: 'hash'
     },
     {
         path: '/learn-to-read/math',
         name: math,
         component: math,
-        mode: 'hash'
     },
     {
         path: '/learn-to-read/settings',
         name: settings,
         component: settings,
-        mode: 'hash'
     },
 
 ]
 
 const router = createRouter({
+    mode: 'hash',
     history: createWebHistory(),
     routes
 })
