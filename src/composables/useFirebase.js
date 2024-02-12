cc00f5d9447caf57c9a606f4c541877f4fe98233
Chen Prom <cprom@Chens-MBP.lan>
2024-02-12 15:21:48 -0600

    add firebase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbTa9BhO58ahcwiDFnK5EwlviueLRyr-U",
+  authDomain: "brain-bright.firebaseapp.com",
+  projectId: "brain-bright",
+  storageBucket: "brain-bright.appspot.com",
+  messagingSenderId: "517459296528",
+  appId: "1:517459296528:web:1a5e0ff36eacb311b9a595"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
