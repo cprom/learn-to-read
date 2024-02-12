62a9a24a3eb252bfcdbde98d85589d9639797351
Chen Prom <cprom@Chens-MBP.lan>
2024-02-12 17:40:16 -0600

    implemented firebase login/authentication

--- src/components/Nav/Navbar.vue
+++ src/components/Nav/Navbar.vue
@@ -1,15 +1,28 @@
 <script setup>
 import './Navbar.css'
+
+import { useAuth } from '../../composables/useAuth'
+const { isAuthenticated, logout, user } = useAuth()
+
+console.log(useAuth)
+
 </script>
 
 <template>
    <nav class="nav-container">
-        <router-link class="home-icon" to="/"><font-awesome-icon class="brain-icon" icon="fa-solid fa-brain" size="2xl" style="color: #FFD43B ;" /><font-awesome-icon class="lightbulb-icon" icon="fa-solid fa-lightbulb" size="2xl" style="color: #63E6BE;" /></router-link>
+        <RouterLink class="home-icon" to="/"><font-awesome-icon class="brain-icon" icon="fa-solid fa-brain" size="2xl" style="color: #FFD43B ;" /><font-awesome-icon class="lightbulb-icon" icon="fa-solid fa-lightbulb" size="2xl" style="color: #63E6BE;" /></RouterLink>
          <div class="nav-btns">
-            <button class="reading button"><router-link to="/read"><font-awesome-icon icon="fa-solid fa-book" size="xl" style="color: #63E6BE;" /> Reading</router-link></button>
-            <button class="writing button"><router-link to="/write"><font-awesome-icon icon="fa-solid fa-pen" size="xl" style="color: #B197FC;" /> Writing</router-link></button>
-            <button class="math button"><router-link to="/math"><font-awesome-icon icon="fa-solid fa-square-root-variable" size="xl" style="color: #ff8ae9;" /> Math</router-link></button>
+            <button class="reading button"><RouterLink to="/read"><font-awesome-icon icon="fa-solid fa-book" size="xl" style="color: #63E6BE;" /> Reading</RouterLink></button>
+            <button class="writing button"><RouterLink to="/write"><font-awesome-icon icon="fa-solid fa-pen" size="xl" style="color: #B197FC;" /> Writing</RouterLink></button>
+            <button class="math button"><RouterLink to="/math"><font-awesome-icon icon="fa-solid fa-square-root-variable" size="xl" style="color: #ff8ae9;" /> Math</RouterLink></button>
          </div>
-         <div class="setting-icon-container"> <router-link to="/settings"><font-awesome-icon class="setting-icon" icon="fa-solid fa-gear" size="2xl" style="color: #B197FC;" /></router-link></div>
+         <div class="setting-icon-container"> <RouterLink to="/settings"><font-awesome-icon class="setting-icon" icon="fa-solid fa-gear" size="2xl" style="color: #B197FC;" /></RouterLink></div>
+         <div v-if="isAuthenticated">
+            Welcome {{ user.email }}
+            <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
+         </div>
+         <div v-else>
+            <RouterLink to="/loginPage">Login</RouterLink>
+         </div>
    </nav>
 </template>
---
+++ src/router.js
@@ -0,0 +1,76 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import read from './views/read.vue'
+import home from './views/home.vue'
+import math from './views/math.vue'
+import write from './views/write.vue'
+import loginPage from './views/loginPage.vue'
+import settings from './views/settings.vue'
+
+import { firebaseApp } from './composables/useFirebase'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
+
+const auth = getAuth(firebaseApp)
+
+const { isAuthenticated, user } = firebaseAuth(auth)
+
+
+const routes = [
+    {
+        path: `/`,
+        name: 'home',
+        component: home,
+        meta: {requiresAuth: true}
+    },
+    
+    {
+        path: '/read',
+        name: 'read',
+        component: read,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/write',
+        name: 'write',
+        component: write,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/math',
+        name: 'math',
+        component: math,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/settings',
+        name: 'settings',
+        component: settings,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/loginPage',
+        name: 'loginPage',
+        component: loginPage,
+    },
+
+]
+
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+//navigation guard
+router.beforeEach((to, _, next) => {
+    if (to.meta.requiresAuth && !isAuthenticated.value) {
+      next({name: 'loginPage', query: {redirect: to.fullPath}})
+    } else {
+      next()
+    }
+  })
+
+
+export default router
